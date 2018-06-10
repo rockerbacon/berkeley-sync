819b69349550ac11ec9818e3ae4d2a1a8c6c3ee0
rockerbacon <mp8andrade@hotmail.com>
2018-06-10 16:42:28 -0300

    Implemented clock for testing synchronization

---
+++ tests/TestClock.java
@@ -0,0 +1,43 @@
+package tests;
+
+public class TestClock {
+
+	private long timestamp;
+	private long updateInterval;
+	private long updateIncrement;
+	private boolean isRunning;
+
+	public TestClock (long beginningTime, long updateInterval, long updateIncrement) {
+		this.timestamp = beginningTime;
+		this.updateInterval = updateInterval;
+		this.updateIncrement = updateIncrement;
+		this.isRunning = false;
+	}
+	
+	public long getTime () {
+		return this.timestamp;
+	}
+	
+	public void adjustTime (long newTime) {
+		this.timestamp = newTime;
+	}
+	
+	public void start () {
+		this.isRunning = true;
+		new Thread ( new Runnable () { @Override public void run () {
+			while (TestClock.this.isRunning) {
+				try {
+					Thread.sleep(TestClock.this.updateInterval);
+					TestClock.this.timestamp += TestClock.this.updateIncrement;
+				} catch (InterruptedException e) {
+					System.err.println("Clock interrupted");
+				}
+			}
+		}).start();
+	}
+	
+	public void stop () {
+		this.isRunning = false;
+	}
+
+}
