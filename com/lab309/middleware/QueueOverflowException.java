438e6135eee973642e3147d85a147272d7335405
rockerbacon <mp8andrade@hotmail.com>
2018-06-10 16:24:26 -0300

    Removidos arquivos desnecessarios

--- com/lab309/middleware/QueueOverflowException.java
+++
@@ -1,7 +0,0 @@
-package com.lab309.middleware;
-
-public class QueueOverflowException extends Exception {
-	public QueueOverflowException () {
-		super("Queue for RPC is full and cannot answer another command. Increase queue size or check that all calls are returning properly");
-	}
-}
